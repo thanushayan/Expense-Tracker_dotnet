56cc030616b74f2a61f0882aea78b7356c569fc1
thanushayan <thanushayan17@gmail.com>
2024-10-20 09:22:40 +0530

    accumilation chart and spline chart

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -35,7 +35,7 @@ namespace Expense_Tracker
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
             app.Run();
         }
